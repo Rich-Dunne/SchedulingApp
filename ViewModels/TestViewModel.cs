2ead2010044b08522c4c8c3f0b1d14f32a8a2227
Rich Dunne <richdunne@yahoo.com>
2021-10-04 06:47:57 -0600

    Added navigation logic

--- ViewModels/TestViewModel.cs
+++ ViewModels/TestViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SchedulingApp.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,37 @@ using System.Threading.Tasks;
 
 namespace SchedulingApp.ViewModels
 {
-    public class TestViewModel
+    public class TestViewModel : ObservableObject
     {
+        private object _currentView;
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set
+            {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        #region Commands
+        public RelayCommand TestViewCommand { get; set; }
+        public RelayCommand HomeViewCommand { get; set; }
+        #endregion
+
+        public TestViewModel()
+        {
+            TestViewCommand = new RelayCommand(o => { NavigateTestView((MainViewModel)o); });
+            HomeViewCommand = new RelayCommand(o => { NavigateHomeView((MainViewModel)o); });
+        }
+        private void NavigateTestView(MainViewModel mainViewModel)
+        {
+            mainViewModel.CurrentView = mainViewModel.TestViewModel;
+        }
+
+        private void NavigateHomeView(MainViewModel mainViewModel)
+        {
+            mainViewModel.CurrentView = mainViewModel.HomeViewModel;
+        }
     }
 }
