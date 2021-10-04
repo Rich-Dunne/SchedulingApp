da59eb732474660116895ff16520075c3b3119fa
Rich Dunne <richdunne@yahoo.com>
2021-10-04 06:48:12 -0600

    Added logic for exit button

--- Views/HomeView.xaml.cs
+++ Views/HomeView.xaml.cs
@@ -1,17 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace SchedulingApp.Views
 {
@@ -23,6 +13,11 @@ namespace SchedulingApp.Views
         public HomeView()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.MainWindow.Close();
         }
     }
 }
