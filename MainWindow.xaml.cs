9a8a41d72763de5a525b3f765cb170f31c9628dc
Rich Dunne <richdunne@yahoo.com>
2021-10-05 07:01:52 -0600

    Added WindowButtonTheme

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Windows;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace SchedulingApp
 {
@@ -27,8 +15,13 @@ namespace SchedulingApp
         private void Window_MouseDown(object sender, MouseButtonEventArgs e)
         {
             if (e.ChangedButton == MouseButton.Left)
+            {
                 DragMove();
+            }
         }
 
+        private void Button_Exit(object sender, RoutedEventArgs e) => Application.Current.MainWindow.Close();
+
+        private void Button_Minimize(object sender, RoutedEventArgs e) => Application.Current.MainWindow.WindowState = WindowState.Minimized;
     }
 }
