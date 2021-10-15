f750b8dade0e0b3a9f5e16674c509626c9c29a3f
Rich Dunne <richdunne@yahoo.com>
2021-10-14 20:16:46 -0600

    Initial commit

---
+++ ViewModels/CalendarViewModel.cs
@@ -0,0 +1,26 @@
+﻿using SchedulingApp.Models;
+using SchedulingApp.Utilities;
+
+namespace SchedulingApp.ViewModels
+{
+    public class CalendarViewModel : ObservableObject
+    {
+        #region View Properties
+
+        #endregion
+
+        #region Commands
+        public RelayCommand TestViewCommand { get; set; }
+        public RelayCommand HomeViewCommand { get; set; }
+        public RelayCommand LoginViewCommand { get; set; }
+        public RelayCommand CalendarViewCommand { get; set; }
+        #endregion
+
+        public CalendarViewModel()
+        {
+            HomeViewCommand = new RelayCommand(o => NavigationService.NavigateTo<HomeViewModel>());
+            LoginViewCommand = new RelayCommand(o => NavigationService.NavigateTo<LoginViewModel>());
+            CalendarViewCommand = new RelayCommand(o => NavigationService.NavigateTo<CalendarViewModel>());
+        }
+    }
+}
---
+++ Views/CalendarView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace SchedulingApp.Views
+{
+    /// <summary>
+    /// Interaction logic for TestView.xaml
+    /// </summary>
+    public partial class CalendarView : UserControl
+    {
+        public CalendarView()
+        {
+            InitializeComponent();
+        }
+    }
+}
